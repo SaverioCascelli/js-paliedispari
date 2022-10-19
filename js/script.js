d83020498aef3c3d0b5651648eedc16f01ae8448
SaverioCascelli <saverio.cascelli@gmail.com>
2022-10-19 16:00:26 +0200

    [add] js comments

--- js/script.js
+++ js/script.js
@@ -15,8 +15,13 @@ let palindromeWord = "asaa";
 console.log(randomGen(1,5));
 
 
+
+//given a string of 1 word, return true if palindrome or false if is not
 function checkPalindrome(word){
 
+    // set up two counter:  i at 0 (first character of the word)    who gets a +1 in every cicle
+    //                      j at the last char of the word          who gets a -1 in every cicle
+    //compare the extrem char of the word and found out if the word is palindrome
     for( let i = 0, j = word.length-1; i < word.length / 2; i++, j--){
         if(word[i] !== word[j]){
             return false;
@@ -25,6 +30,7 @@ function checkPalindrome(word){
     return true;
 }
 
+//returning a random number between min e max. Both min and max included
 function randomGen(min , max){
     return Math.floor(Math.random() * (max - min + 1) + min);
 }
